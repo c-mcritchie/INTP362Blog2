1558fc68678d945fbe0be31afbc4091d8d6a75a0
cmbackflip <colem1700@gmail.com>
2023-04-06 16:25:34 -0600

    add typescript and clean up template

--- objectcounter/client/src/main.ts
+++ objectcounter/client/src/main.ts
@@ -1,12 +1,10 @@
 import './global.css';
 
+// @ts-ignore
 import App from './App.svelte';
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
 });
 
 export default app;
